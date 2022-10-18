2a0b177a0c354d50b433ce0ee1107c90f595ee15
dlfud <99ysl@naver.com>
2022-10-18 11:30:43 +0900

    대댓글 조회

--- db.sql
+++ db.sql
@@ -2,6 +2,7 @@ DROP DATABASE IF EXISTS horang;
 CREATE DATABASE horang;
 USE horang;
 
+#익명게시판
 CREATE TABLE post(
     id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
@@ -10,6 +11,8 @@ CREATE TABLE post(
     `view` INT(10) DEFAULT 0
 );
 
+
+#비밀게시판
 DROP TABLE secretPost;
 CREATE TABLE secretPost(
     id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -19,6 +22,7 @@ CREATE TABLE secretPost(
     `view` INT(10) DEFAULT 0
 );
 
+#회원
 CREATE TABLE `member`(
     id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     memberId TEXT NOT NULL,
@@ -26,7 +30,7 @@ CREATE TABLE `member`(
     email TEXT NOT NULL
 );
 
-
+#익명게시판 댓글
 CREATE TABLE postComment(
     postCommentId INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     postCommentContent TEXT NOT NULL,
@@ -35,6 +39,19 @@ CREATE TABLE postComment(
     FOREIGN KEY(post_id) REFERENCES Post(id) ON DELETE CASCADE
 );
 
+#익명게시판 대댓글
+DROP TABLE postCommentComment;
+CREATE TABLE postCommentComment(
+    postCommentCommentId INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    postCommentCommentContent TEXT NOT NULL,
+    postCommentCommentCreateDate DATETIME DEFAULT NOW(),
+    postComment_id INT(11) UNSIGNED NOT NULL,
+    FOREIGN KEY(postComment_id) REFERENCES PostComment(postCommentId) ON DELETE CASCADE,
+    post_id INT(11) UNSIGNED NOT NULL,
+    FOREIGN KEY(post_id) REFERENCES Post(id) ON DELETE CASCADE
+);
+
+#비밀 게시판 댓글
 DROP TABLE  secretPostComment;
 CREATE TABLE secretPostComment(
     secretPostCommentId INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -44,10 +61,24 @@ CREATE TABLE secretPostComment(
     FOREIGN KEY(secretPost_id) REFERENCES secretPost(id) ON DELETE CASCADE
 );
 
+#비밀게시판 대댓글
+DROP TABLE secretPostCommentComment;
+CREATE TABLE secretPostCommentComment(
+    secretPostCommentCommenttId INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    secretPostCommentCommentContent TEXT NOT NULL,
+    secretPostCommentCommentCreateDate DATETIME DEFAULT NOW(),
+    secretPostComment_id INT(11) UNSIGNED NOT NULL,
+    FOREIGN KEY(secretPostComment_id) REFERENCES secretPostComment(secretPostCommentId) ON DELETE CASCADE,
+    secretPost_id INT(11) UNSIGNED NOT NULL,
+    FOREIGN KEY(secretPost_id) REFERENCES secretPost(id) ON DELETE CASCADE
+);
+
 SELECT * FROM post;
 SELECT * FROM secretPost;
 SELECT * FROM secretPostComment;
 SELECT * FROM postComment;
+SELECT * FROM secretPostCommentComment;
+SELECT * FROM postCommentComment;
 
 INSERT INTO Post
 SET title="익명 게시판",
@@ -57,8 +88,8 @@ INSERT INTO secretPost
 SET title="님 대가리 깨버릴거임",
 content="너무해ㅠㅠ";
 
-INSERT INTO secretPostComment
-SET secretPostCommentContent="너무해ㅠㅠ", secretPost_id = 1;
+INSERT INTO postComment
+SET postCommentContent="너무해ㅠㅠ", post_id = 19;
 
 
 SELECT * FROM secretPost a LEFT JOIN `comment`b ON a.id = b.secretPost_id WHERE a.id = 160;
