015599e7de6fcc307986368c6616fdef08b40ae7
dlfud <99ysl@naver.com>
2022-11-04 13:49:27 +0900

    삭제시 확인

--- client/src/component/detailPage/DetailPage.jsx
+++ client/src/component/detailPage/DetailPage.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Comment from "../comment/Comment.jsx";
-import {url} from "../../configIp";
+import { url } from "../../configIp";
 
 const DetailPage = ({ method, id }) => {
   const [activity, setActivity] = useState(0);
@@ -50,46 +50,63 @@ const DetailPage = ({ method, id }) => {
   return (
     <>
       <div>
-        <strong className="text-2xl inline-block"><div>{detail.title}</div></strong>
+        <strong className="text-2xl inline-block">
+          <div>{detail.title}</div>
+        </strong>
         <div className="inline-block float-right">조회수 : {detail.view}</div>
-        <hr/>
+        <hr />
         <div>
           <div className="p-9">{detail.content}</div>
-          <hr/>
+          <hr />
           <div className="inline-block p-3 float-right">
-            <button onClick={loginout} className="float-left border-2 mr-2">목록</button>
+            <button onClick={loginout} className="float-left border-2 mr-2">
+              목록
+            </button>
 
-            {onoff?
-            <>
-              <Link to={`/${sort}Update/${id}`} className="float-left border-2 mr-2">수정</Link>
-              <form
-                onSubmit={async (e) => {
-                  e.preventDefault();
-                  await axios({
-                    url: `${url}/${sort}Delete/${id}`,
-                    method: "POST",
-                  });
-                  navigate("/home");
-                }}
-                className="float-left border-2"
-              >
-                <button>삭제</button>
-              </form>
-            </>
-            : null
-            }
+            {onoff ? (
+              <>
+                <Link to={`/${sort}Update/${id}`} className="float-left border-2 mr-2">
+                  수정
+                </Link>
+                <div
+                  onSubmit={async (e) => {
+                    e.preventDefault();
+                    await axios({
+                      url: `${url}/${sort}Delete/${id}`,
+                      method: "POST",
+                    });
+                    navigate("/home");
+                  }}
+                  className="float-left border-2"
+                >
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      if (window.confirm("삭제 할거니?")) {
+                        const checkDelete = async () => {
+                          await axios({
+                            url: `${url}/${sort}Delete/${id}`,
+                            method: "POST",
+                          });
+                          navigate("/home");
+                        };
+                        checkDelete();
+                      } else {
+                        alert("취소");
+                      }
+                    }}
+                  >
+                    삭제
+                  </button>
+                </div>
+              </>
+            ) : null}
           </div>
-          
+
           <div className="mt-12">
             <div>댓글</div>
-            <hr/>
-            <Comment
-              sort={sort}
-              activity={activity}
-              setActivity={setActivity}
-              id={id}
-              onoff={onoff}
-            />
+            <hr />
+            <Comment sort={sort} activity={activity} setActivity={setActivity} id={id} onoff={onoff} />
           </div>
         </div>
       </div>
