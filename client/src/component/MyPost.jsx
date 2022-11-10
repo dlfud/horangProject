94d1d610efacc94ddda00cb4b050c01642eed811
Parkseonho <tjsgh2103@gmail.com>
2022-11-10 11:27:07 +0900

    mypost에 nav 추가

--- client/src/component/MyPost.jsx
+++ client/src/component/MyPost.jsx
@@ -53,96 +53,105 @@ const MyPost = () => {
 
   return (
     <>
-      <div className="container mx-auto mt-12 ">
-        <div className="grid ">
-          <div className="my-12">
-            <div className="text-4xl">익명게시판</div>
-            {myPosts.length !== 0 ? (
-              <>
-                <div className="overflow-auto border-white border-b-8 pb-10">
-                  <table className="mt-5">
-                    <thead className="bg-gray-50">
-                      <tr className="whitespace-nowrap">
-                        <th className="px-6 py-2 text-xs text-gray-500 ">No</th>
-                        <th className="px-6 py-2 text-xs text-gray-500">제목</th>
-                        <th className="px-6 py-2 text-xs text-gray-500">작성일</th>
-                        <th className="px-6 py-2 text-xs text-gray-500">조회수</th>
-                      </tr>
-                    </thead>
-
-                    {myPosts.slice(offset, offset + limit).map((myPost, index) => (
-                      <tbody key={index} className="bg-white divide-y divide-gray-300 border">
-                        <tr className="whitespace-nowrap">
-                          <td className="px-6 py-4 text-sm text-gray-500 text-center tdWidth">{myPost.id}</td>
-                          <td className="px-6 py-4 TitleWidth">
-                            <Link to={`/postDetailPage/${myPost.id}`}>{myPost.title}</Link>
-                            {/* <span className="text-violet-300 ml-3 tdWidth">{countPostComment(myPost.id)}</span> */}
-                          </td>
-                          <td className="px-6 py-4 text-sm text-center tdWidth ">
-                            {moment(myPost.create_date).format("YY.MM.DD")}
-                          </td>
-                          <td className="px-6 py-4  text-sm  text-center tdWidth">{myPost.view}</td>
-                        </tr>
-                      </tbody>
-                    ))}
-                  </table>
-                  <div className=" text-center">
-                    <Pagination total={myPosts.length} limit={limit} page={page} setPage={setPage} />
-                  </div>
-                </div>
-              </>
-            ) : (
-              <div className="bg-white divide-y divide-gray-300 border postOXWidth mt-5 text-center mx-auto">
-                익명게시물이 없습니다.
-              </div>
-            )}
+      <div className="flex Main">
+        <div className=" px-3 Side w-70">
+          <div className="space-y-3">
+            <div className="flex-1">
+              <Nav onoff={onoff} />
+            </div>
           </div>
-          <div>
-            <div className="text-4xl">비밀게시판</div>
-            {mySecretPosts.length !== 0 ? (
-              <>
-                <div className="overflow-auto ">
-                  <table className="mt-5">
-                    <thead className="bg-gray-50">
-                      <tr className="whitespace-nowrap">
-                        <th className="px-6 py-2 text-xs text-gray-500 ">No</th>
-                        <th className="px-6 py-2 text-xs text-gray-500">제목</th>
-                        <th className="px-6 py-2 text-xs text-gray-500">작성일</th>
-                        <th className="px-6 py-2 text-xs text-gray-500">조회수</th>
-                      </tr>
-                    </thead>
-
-                    {mySecretPosts.map((mySecretPost, index) => (
-                      <tbody key={index} className="bg-white divide-y divide-gray-300 border">
+        </div>
+        <div className="container mx-auto mt-12 ">
+          <div className="grid ">
+            <div className="my-12">
+              <div className="text-4xl">익명게시판</div>
+              {myPosts.length !== 0 ? (
+                <>
+                  <div className="overflow-auto border-white border-b-8 pb-10">
+                    <table className="mt-5">
+                      <thead className="bg-gray-50">
                         <tr className="whitespace-nowrap">
-                          <td className="px-6 py-4 text-sm text-gray-500 text-center tdWidth">{mySecretPost.id}</td>
-                          <td className="px-6 py-4 TitleWidth">
-                            <Link to={`/postDetailPage/${mySecretPost.id}`}>{mySecretPost.title}</Link>
-                            {/* <span className="text-violet-300 ml-3 tdWidth">{countPostComment(myPost.id)}</span> */}
-                          </td>
-                          <td className="px-6 py-4 text-sm text-center tdWidth ">
-                            {moment(mySecretPost.create_date).format("YY.MM.DD")}
-                          </td>
-                          <td className="px-6 py-4  text-sm  text-center tdWidth">{mySecretPost.view}</td>
+                          <th className="px-6 py-2 text-xs text-gray-500 ">No</th>
+                          <th className="px-6 py-2 text-xs text-gray-500">제목</th>
+                          <th className="px-6 py-2 text-xs text-gray-500">작성일</th>
+                          <th className="px-6 py-2 text-xs text-gray-500">조회수</th>
                         </tr>
-                      </tbody>
-                    ))}
-                  </table>
-                  <div className=" text-center">
-                    <SecretPagination
-                      total={mySecretPosts.length}
-                      limit={limit}
-                      secretPage={secretPage}
-                      setSecretPage={setSecretPage}
-                    />
+                      </thead>
+
+                      {myPosts.slice(offset, offset + limit).map((myPost, index) => (
+                        <tbody key={index} className="bg-white divide-y divide-gray-300 border">
+                          <tr className="whitespace-nowrap">
+                            <td className="px-6 py-4 text-sm text-gray-500 text-center tdWidth">{myPost.id}</td>
+                            <td className="px-6 py-4 TitleWidth">
+                              <Link to={`/postDetailPage/${myPost.id}`}>{myPost.title}</Link>
+                              {/* <span className="text-violet-300 ml-3 tdWidth">{countPostComment(myPost.id)}</span> */}
+                            </td>
+                            <td className="px-6 py-4 text-sm text-center tdWidth ">
+                              {moment(myPost.create_date).format("YY.MM.DD")}
+                            </td>
+                            <td className="px-6 py-4  text-sm  text-center tdWidth">{myPost.view}</td>
+                          </tr>
+                        </tbody>
+                      ))}
+                    </table>
+                    <div className=" text-center">
+                      <Pagination total={myPosts.length} limit={limit} page={page} setPage={setPage} />
+                    </div>
                   </div>
+                </>
+              ) : (
+                <div className="bg-white divide-y divide-gray-300 border postOXWidth mt-5 text-center mx-auto">
+                  익명게시물이 없습니다.
                 </div>
-              </>
-            ) : (
-              <div className="bg-white divide-y divide-gray-300 border postOXWidth mt-5 text-center mx-auto">
-                비밀게시물이 없습니다.
-              </div>
-            )}
+              )}
+            </div>
+            <div>
+              <div className="text-4xl">비밀게시판</div>
+              {mySecretPosts.length !== 0 ? (
+                <>
+                  <div className="overflow-auto ">
+                    <table className="mt-5">
+                      <thead className="bg-gray-50">
+                        <tr className="whitespace-nowrap">
+                          <th className="px-6 py-2 text-xs text-gray-500 ">No</th>
+                          <th className="px-6 py-2 text-xs text-gray-500">제목</th>
+                          <th className="px-6 py-2 text-xs text-gray-500">작성일</th>
+                          <th className="px-6 py-2 text-xs text-gray-500">조회수</th>
+                        </tr>
+                      </thead>
+
+                      {mySecretPosts.map((mySecretPost, index) => (
+                        <tbody key={index} className="bg-white divide-y divide-gray-300 border">
+                          <tr className="whitespace-nowrap">
+                            <td className="px-6 py-4 text-sm text-gray-500 text-center tdWidth">{mySecretPost.id}</td>
+                            <td className="px-6 py-4 TitleWidth">
+                              <Link to={`/postDetailPage/${mySecretPost.id}`}>{mySecretPost.title}</Link>
+                              {/* <span className="text-violet-300 ml-3 tdWidth">{countPostComment(myPost.id)}</span> */}
+                            </td>
+                            <td className="px-6 py-4 text-sm text-center tdWidth ">
+                              {moment(mySecretPost.create_date).format("YY.MM.DD")}
+                            </td>
+                            <td className="px-6 py-4  text-sm  text-center tdWidth">{mySecretPost.view}</td>
+                          </tr>
+                        </tbody>
+                      ))}
+                    </table>
+                    <div className=" text-center">
+                      <SecretPagination
+                        total={mySecretPosts.length}
+                        limit={limit}
+                        secretPage={secretPage}
+                        setSecretPage={setSecretPage}
+                      />
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <div className="bg-white divide-y divide-gray-300 border postOXWidth mt-5 text-center mx-auto">
+                  비밀게시물이 없습니다.
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
